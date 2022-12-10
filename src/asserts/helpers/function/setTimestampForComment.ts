da3abaa1f38f40dab232eeebeaefbcfb1d2e1dea
Lucass9 <l_pisarek@o2.pl>
2022-12-10 10:44:31 +0100

    fix show time

--- src/asserts/helpers/function/setTimestampForComment.ts
+++ src/asserts/helpers/function/setTimestampForComment.ts
@@ -4,26 +4,26 @@ export const setTimestamForComment = (postTimestamp: number | undefined, timesta
     const rtf1 = new Intl.RelativeTimeFormat('en', { style: 'long' });
     let formatTime: Intl.RelativeTimeFormatUnit = 'second'
     let timeValue
-    if (postTimestamp !== undefined) timeValue = Math.floor((postTimestamp - timestamp) / 1000)
+    if (postTimestamp !== undefined) timeValue = Math.ceil((postTimestamp - timestamp) / 1000)
     if (timeValue !== undefined) {            
             if (Math.abs(timeValue) >= 60) {
                 formatTime = "minute"
-                timeValue = Math.floor(timeValue / 60)
+                timeValue = Math.ceil(timeValue / 60)
                 if (Math.abs(timeValue) >= 60) {
                     formatTime = "hour"
-                    timeValue = Math.floor(timeValue / 60)
+                    timeValue = Math.ceil(timeValue / 60)
                     if (Math.abs(timeValue) >= 24) {
                         formatTime = "day"
-                        timeValue = Math.floor(timeValue / 24)
+                        timeValue = Math.ceil(timeValue / 24)
                         if (Math.abs(timeValue) >= 7) {
                             formatTime = "week"
-                            timeValue = Math.floor(timeValue / 7)
+                            timeValue = Math.ceil(timeValue / 7)
                             if (Math.abs(timeValue) >= 4) {
                                 formatTime = "month"
-                                timeValue = Math.floor(timeValue / 4)
+                                timeValue = Math.ceil(timeValue / 4)
                                 if (Math.abs(timeValue) >= 12) {
                                     formatTime = "year"
-                                    timeValue = Math.floor(timeValue / 12)
+                                    timeValue = Math.ceil(timeValue / 12)
                                 }
                             }
                         }
